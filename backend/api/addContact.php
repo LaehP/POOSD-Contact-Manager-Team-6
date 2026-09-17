94dcb4b7e1dd2e60d94868e6158fa8e020baa725
mikaylaphilpot <mikaylagp@hotmail.com>
2026-09-17 12:38:49 -0400

    Get addContact.php fully working

--- backend/api/addContact.php
+++ backend/api/addContact.php
@@ -7,11 +7,14 @@
 
     $conn = new mysqli('localhost', 'root', 'password', 'SmallProject');
 
-    $inData = getRequestInfo();
-    if ((!array_key_exists($inData, "firstName") || !array_key_exists($inData, "lastName")) && !array_key_exists($inData, "phoneNumber") && !array_key_exists($inData, "email")) {
+    $inData = getRequestInfo(); 
+    if ($inData === null) {
+        returnWithError("JSON decode failed");
+    }
+    if (!array_key_exists("firstName", $inData) && !array_key_exists("lastName", $inData) && !array_key_exists("phoneNumber", $inData) && !array_key_exists("email", $inData)) {
         returnWithError("Minimum contact information not defined. Must include first name, last name, phone number, and email.");
     }
-    if (!array_key_exists($inData, "userId")) {
+    if (!array_key_exists("userId", $inData)) {
         returnWithError("User ID must be defined");
     }
     $contactFirstName = $inData["firstName"];
@@ -27,18 +30,8 @@
 
     }
     else {
-        if (array_key_exists($inData, "firstName") && array_key_exists($inData, "lastName")) {
-            $contactInsertion = $conn->prepare("INSERT into CONTACTS (FirstName, LastName, Phone, Email, UserID) VALUES(?, ?, ?, ?, ?) ");
-            $contactInsertion->bind_param("ssssi", $contactFirstName, $contactLastName, $contactPhoneNumber, $contactEmail, $userId);
-        }
-        else if (array_key_exists($inData, "firstName") && !array_key_exists($inData, "lastName")) {
-            $contactInsertion = $conn->prepare("INSERT into CONTACTS (FirstName, Phone, Email, UserID) VALUES(?, ?, ?, ?) ");
-            $contactInsertion->bind_param("sssi", $contactFirstName, $contactPhoneNumber, $contactEmail, $userId);
-        }
-        else {
-            $contactInsertion = $conn->prepare("INSERT into CONTACTS (LastName, Phone, Email, UserID) VALUES(?, ?, ?, ?) ");
-            $contactInsertion->bind_param("sssi", $contactLasttName, $contactPhoneNumber, $contactEmail, $userId);
-        }
+        $contactInsertion = $conn->prepare("INSERT into CONTACTS (FirstName, LastName, Phone, Email, UserID) VALUES(?, ?, ?, ?, ?) ");
+        $contactInsertion->bind_param("ssssi", $contactFirstName, $contactLastName, $contactPhoneNumber, $contactEmail, $userId);
         $contactInsertion->execute();
         $contactInsertion->close();
         $conn->close();
