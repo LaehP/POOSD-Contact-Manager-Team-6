8b00a2db85f18614980550ef80f7c7f71eec33b9
Jonathon Faircloth <jmf2005.jf@gmail.com>
2026-09-18 10:58:59 -0400

    Adds functionality to include last name and or full name matching in searchContact.php

--- backend/api/searchContact.php
+++ backend/api/searchContact.php
@@ -1,6 +1,10 @@
 <?php
-    // search contacts in the database with the same first name given by the user and there contacts and return each contact's FistName, LastName, PhoneNumber. This will allow the user to select which contact they want to view. 
-    
+    // server side search, no shared contacts
+    // search contacts in the database with the same first name and or last name given by the user and there contacts and return each contact's FistName, LastName, PhoneNumber. This will allow the user to select which contact they want to view. 
+    // (partial matching) if user searches "Jo" the search algorithm should match everything with "Jo" in it (case insensitive) ie, John, Jones, Jobs
+    // 
+
+
     // Get the request data
     $inData = getRequestInfo();
 
@@ -10,6 +14,7 @@
         returnWithError("All fields are required");
         exit;
     }
+
 
     // parse the .env file for database connection parameters
     $envPath = __DIR__ . '/../../.env';
@@ -61,12 +66,18 @@
     } 
     else
     {   
-        // Search for the contact in the database and show all information about the contact by matching information given by the user. This is to confirm that the contact exists and is associated with the user before showing the contact.
+        // Search for the contacts in the database and show all information about the contact by matching information given by the user. This is to confirm that the contacts exist and is associated with the user before showing the contacts.
     
-        $stmt = $conn->prepare("select Name from Contacts where Name like ? and UserID=?");
-		$contactName = "%" . $inData["search"] . "%";
-		$stmt->bind_param("ss", $contactName, $inData["userId"]);
-		$stmt->execute();
+        /* 
+        $stmt = $conn->prepare("select FirstName, LastName from Contacts where (FirstName like ? or LastName like ?) and UserID=?");
+		$search = "%" . $inData["search"] . "%";
+		$stmt->bind_param("ss", $search, $search, $inData["userId"]);
+		*/
+        
+        $stmt = $conn->prepare("SELECT FirstName, LastName FROM Contacts WHERE (FirstName LIKE ? OR LastName LIKE ? OR CONCAT_WS(' ', FirstName, LastName) LIKE ? ) AND UserID = ?");
+        $search = "%" . trim($inData["search"]) . "%";
+        $stmt->bind_param( "ssss", $search, $search, $search, $inData["userId"] );
+        $stmt->execute();
 		
 		$result = $stmt->get_result();
 		
@@ -77,7 +88,7 @@
 				$searchResults .= ",";
 			}
 			$searchCount++;
-			$searchResults .= '"' . $row["Name"] . '"';
+			$searchResults .= '"' . $row["FirstName"] . ' ' . $row["LastName"] . '"';
 		}
 		
 		if( $searchCount == 0 )
@@ -91,6 +102,7 @@
 		
 		$stmt->close();
 		$conn->close();
+        
     }
 
     function getRequestInfo()
