6787cc59ef110efef8ff8018d887d1109bd0e3a8
mikaylaphilpot <mikaylagp@hotmail.com>
2026-09-19 14:58:43 -0400

    Update viewContacts.php to connect to database via shared .env function and have more diverse response codes and error checks

--- backend/api/viewContacts.php
+++ backend/api/viewContacts.php
@@ -2,21 +2,29 @@
     require_once "databaseConnection.php";
     require_once "frontendInfo.php";
 
-    $conn = new mysqli('localhost', 'root', 'password', 'SmallProject');
-    $inData = getRequestInfo();
+    $conn = connectToDatabase();
     if (empty($_GET['userId'])) {
+        http_response_code(400);
         returnWithError("User ID must be defined");
+        exit;
     }
     $userId = $_GET['userId'];
     if ($conn->connect_error) 
     {
+        http_response_code(500);
         returnWithError( $conn->connect_error );
-
+        exit;
     }
     else {
         $pullContacts = $conn->prepare("SELECT FirstName, LastName, ID FROM CONTACTS WHERE UserID = ?");
         $pullContacts->bind_param("i", $userId);
-        $pullContacts->execute();
+        if (!($pullContacts->execute())) {
+            http_response_code(500);
+            returnWithError("Error encountered when pulling contacts.");
+            $pullContacts->close();
+            $conn->close();
+            exit;
+        }
         $result = $pullContacts->get_result();
         $pullContacts->close();
         $conn->close();
